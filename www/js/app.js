33a571a006c562de75b05a816da4121f56ac05f3
Jonathan Pinlac <jonppinlac@gmail.com>
2016-03-21 15:45:16 -0400

    Initial routing setup and views

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,24 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('macmeal', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.config(function($stateprovider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateprovider
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/home.html',
+      controller: 'homeController'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html'
+    });
+})
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +36,12 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller('homeController', function($scope, calendarService){
+
+});
+
+app.factory('calendarService', function($http){
+
+});
